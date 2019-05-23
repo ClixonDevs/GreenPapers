70741d26891a04fb9ba6a6ea61ff5b1218bd0a2b
Cleax20 <Clemensv20@gmail.com>
2019-05-23 11:12:36 +0200

    V1.1
    Remove Laravel Environment and started out with basic folders for project

--- Project/resources/views/welcome.blade.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <head>
-        <title>A simple website</title>
-    </head>
-    <body>
-        <h1> If you see this, welcome the main page.</h1>
-        <h2> If not, you are joking with me</h2>
-    </body>
-</html>
